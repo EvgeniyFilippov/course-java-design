9c9a934d1d65829e042fbad571f248704f7e6306
Yauhen Filipau <6479556@gmail.com>
2021-06-13 22:36:49 +0300

    add new class pizza

---
+++ src/main/java/com/rakovets/course/design/practice/solid/Pizza.java
@@ -0,0 +1,29 @@
+package main.java.com.rakovets.course.design.practice.solid;
+
+import java.util.Map;
+
+public class Pizza {
+    private String pizzaName;
+    private Map<String, Integer> ingredients;
+
+    public Pizza(String pizzaName, Map<String, Integer> ingredients) {
+        this.pizzaName = pizzaName;
+        this.ingredients = ingredients;
+    }
+
+    public String getPizzaName() {
+        return pizzaName;
+    }
+
+    public void setPizzaName(String pizzaName) {
+        this.pizzaName = pizzaName;
+    }
+
+    public Map<String, Integer> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Map<String, Integer> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
