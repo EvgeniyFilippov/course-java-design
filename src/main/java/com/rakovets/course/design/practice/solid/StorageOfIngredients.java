29a69ff2952d1a991350528aa4bab6243f271d27
Yauhen Filipau <6479556@gmail.com>
2021-06-13 22:39:32 +0300

    add new class StorageOfIngredients.java

---
+++ src/main/java/com/rakovets/course/design/practice/solid/StorageOfIngredients.java
@@ -0,0 +1,19 @@
+package main.java.com.rakovets.course.design.practice.solid;
+
+import java.util.Map;
+
+public class StorageOfIngredients {
+    private Map<String, Integer> ingredients;
+
+    public StorageOfIngredients(Map<String, Integer> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Map<String, Integer> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Map<String, Integer> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
