4878c1b4d4cb6af59eefa0880952112ebad39200
Yauhen Filipau <6479556@gmail.com>
2021-06-14 21:49:27 +0300

    add discount method

---
+++ src/main/java/com/rakovets/course/design/practice/solid/Discount.java
@@ -0,0 +1,9 @@
+package main.java.com.rakovets.course.design.practice.solid;
+
+import java.util.Date;
+
+public class Discount {
+    public float calculateDiscount(int numberOfPizzas, Date date) {
+        return 0.0f;
+    }
+}
