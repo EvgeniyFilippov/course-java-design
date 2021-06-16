2df9d41bb8c50602a6cfc9df98daf4c23ac0f51a
Yauhen Filipau <6479556@gmail.com>
2021-06-16 22:03:33 +0300

    add diablo pizza

--- src/test/java/com/rakovets/course/design/practice/solid/Demo.java
+++ src/test/java/com/rakovets/course/design/practice/solid/Demo.java
@@ -11,6 +11,12 @@ public class Demo {
         ingredientsOfPepperoni.put("salt", 50);
         Pizza pepperoni = new Pizza("Pepperoni", ingredientsOfPepperoni);
 
+        Map<String, Integer> ingredientsOfDiablo = new LinkedHashMap<>();
+        ingredientsOfDiablo.put("cheese", 120);
+        ingredientsOfDiablo.put("pepper", 20);
+        ingredientsOfDiablo.put("salt", 10);
+        Pizza diablo = new Pizza("Diablo", ingredientsOfDiablo);
+
         System.out.println(pepperoni.toString());
 
     }
